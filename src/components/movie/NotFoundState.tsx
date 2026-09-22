f369f4826e499bc8ff0ecd42e05fe4499b386de6
Yusuf-98 <yusuf.smg@gmail.com>
2026-09-22 22:22:32 +0700

    add: not found block labels

--- src/components/movie/NotFoundState.tsx
+++ src/components/movie/NotFoundState.tsx
@@ -5,10 +5,12 @@ export function NotFoundState() {
   return (
     <m.div initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }}>
       <div className="flex flex-col items-center justify-center flex-1 gap-xl pb-68 mt-38 md:mt-55.25">
+        {/* Icon */}
         <div className="w-50 h-50">
           <img src={NotFound} alt="Not Found" className="w-full h-full grayscale-100" />
         </div>
 
+        {/* Message */}
         <div className="flex flex-col items-center gap-md text-center">
           <p className="text-base-white font-semibold text-size-md">Data Not Found</p>
           <p className="text-neutral-400 text-size-sm">Try other keywords</p>
